8bf1c6d06a46c57488211e746c6b2e1f2bc24763
Gerardo <g.socias29@gmail.com>
2024-07-24 02:15:45 +0800

    fix

--- app/Http/Controllers/LeagueController.php
+++ app/Http/Controllers/LeagueController.php
@@ -42,9 +42,10 @@ class LeagueController extends Controller
     public function getLeagueById($id) {
         $userId = Auth::user()->id;
     
-        $league = League::with(['league_users.user', 'league_users.rebuys'])
-            ->where('id', $id)
-            ->firstOrFail();
+        // Eager load league_users with the user and filtered rebuys
+        $league = League::with(['league_users.user', 'league_users.rebuys' => function($query) {
+            $query->whereColumn('league_users.user_id');
+        }])->where('id', $id)->firstOrFail();
     
         $league->has_joined = $league->participants()->where('user_id', $userId)->exists();
         if ($league->has_joined) {
@@ -56,7 +57,7 @@ class LeagueController extends Controller
         }
     
         return response()->json($league);
-    }    
+    }
 
     public function index(Request $request) {
         $userId = Auth::user()->id;
