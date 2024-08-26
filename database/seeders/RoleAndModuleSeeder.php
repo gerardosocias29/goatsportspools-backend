9e246bd893a96a0730679c21a7440a5c41c5af2e
Gerardo <g.socias29@gmail.com>
2024-08-27 00:45:57 +0800

    update

--- database/seeders/RoleAndModuleSeeder.php
+++ database/seeders/RoleAndModuleSeeder.php
@@ -28,7 +28,7 @@ class RoleAndModuleSeeder extends Seeder
             ['name' => 'Leagues', 'page' => 'leagues', 'icon' => 'icon-leagues'],
 
             ['name' => 'Settings', 'page' => '', 'icon' => 'icon-settings'],
-            // ['name' => 'How It Works', 'page' => 'how-it-works', 'icon' => 'icon-hiw'],
+            ['name' => 'How It Works', 'page' => 'how-it-works', 'icon' => 'icon-hiw'],
 
             ['name' => 'Manage Games', 'page' => 'settings/game-management', 'icon' => 'icon-game-management', 'parent_id' => 6],
             ['name' => 'Manage Teams', 'page' => 'settings/teams', 'icon' => 'icon-teams', 'parent_id' => 6],
@@ -58,7 +58,7 @@ class RoleAndModuleSeeder extends Seeder
             [
                 'name' => 'Normal User',
                 'description' => 'Has access to participate in NFL pool and view scores.',
-                'allowed_modules' => [1,2,4,5] // Assign specific module IDs
+                'allowed_modules' => env('APP_ENV') === "local" ? [1,2,4,5] : [1,7]  // Assign specific module IDs
             ]
         ];
 
