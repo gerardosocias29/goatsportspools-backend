58887a4f317e357843ea6e493627bc08573f1de5
Gerardo <g.socias29@gmail.com>
2024-09-09 02:23:32 +0800

    revert

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -42,15 +42,15 @@ class UserController extends Controller
             
             if ($isInLeague) {
                 // If user is in a league, only show module ids [2, 3, 5]
-                $modules = $modules->filter(function($module) use ($user) {
-                    return in_array($module->id, $user->role_id == 3 ? [1, 2, 3, 4, 6, 9] : [1, 2, 3, 4, 6, 7, 8, 9]);
+                $modules = $modules->filter(function($module) {
+                    return in_array($module->id, [1, 2, 3, 5, 8]);
                 });
                 $modules = $modules->values();
 
             } else {
                 // If user is not in a league, hide module ids [2, 3, 5]
                 $modules = $modules->filter(function($module) {
-                    return !in_array($module->id, [2, 4, 6]);
+                    return !in_array($module->id, [2, 3, 5]);
                 });
                 $modules = $modules->values();
 
